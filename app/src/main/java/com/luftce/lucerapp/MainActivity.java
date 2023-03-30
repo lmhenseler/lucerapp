612b477650912e761656d246a09b4fd0c465db69
Luna <lm.henseler@gmail.com>
2023-03-30 18:45:06 +0200

    Conexion con Firebase

--- app/src/main/java/com/luftce/lucerapp/MainActivity.java
+++ app/src/main/java/com/luftce/lucerapp/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+      //Falta el registro y login
     }
+
+
+
 }
