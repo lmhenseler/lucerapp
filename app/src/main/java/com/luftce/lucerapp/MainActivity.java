dfc02be258b10f164152dd619dfbe6b2cd63bb6f
cesar <cesar.gismera.elias@alumnos.upm.es>
2023-04-12 17:28:07 +0200

    Añado lógica de login en Firebase

--- app/src/main/java/com/luftce/lucerapp/MainActivity.java
+++ app/src/main/java/com/luftce/lucerapp/MainActivity.java
@@ -1,10 +1,23 @@
 package com.luftce.lucerapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
       //Falta el registro y login
     }
 
+    public void loginToFirebase(){
+        EditText userInput = findViewById(R.id.userInput);
+        EditText passwordInput = findViewById(R.id.passwordInput);
+        String email = userInput.getText().toString();
+        String password = passwordInput.getText().toString();
 
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+            @Override
+            public void onComplete(@NotNull Task<AuthResult> task){
+                if (task.isSuccessful()){
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }
+                else {
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
 }
